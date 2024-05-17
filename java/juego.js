0c33c3b2ae538363c9ced9674486452b406e8587
monicabarriosg <monicabarrios0750@gmail.com>
2024-05-17 14:37:56 -0600

    inicio en java

---
+++ java/juego.js
@@ -0,0 +1,45 @@
+// const matriz = [
+// [1,2,3],
+// [4,5,6],
+// [7,8,9]
+// ]
+// console.log(matriz);
+// matriz [0,0] [0,1] = "x"
+// console.log(matriz)
+
+
+// let valor = document.getElementsByClassName("valor");
+// console.log (valor);
+
+// valor[0,0].addEventListener("click", function () {
+//     valor[0,0].innerHTML = "❤️"
+// })  
+// console.log(valor)
+// if (condition) {
+    
+// }
+
+let b00 = document.getElementById("b00");
+let b01 = document.getElementById("b01");
+let b02 = document.getElementById("b02");
+let b10 = document.getElementById("b10");
+let b11 = document.getElementById("b11");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
