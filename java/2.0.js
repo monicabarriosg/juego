aad1bae919bc1a5b0e6f5fdcb543f81b69804823
monicabarriosg <monicabarrios0750@gmail.com>
2024-05-20 10:31:32 -0600

    nuevo java

--- java/2.0.js
+++ java/2.0.js
@@ -1,23 +1,4 @@
-// const matriz = [
-// [1,2,3],
-// [4,5,6],
-// [7,8,9]
-// ]
-// console.log(matriz);
-// matriz [0,0] [0,1] = "x"
-// console.log(matriz)
 
-
-// let valor = document.getElementsByClassName("valor");
-// console.log (valor);
-
-// valor[0,0].addEventListener("click", function () {
-//     valor[0,0].innerHTML = "❤️"
-// })  
-// console.log(valor)
-// if (condition) {
-    
-// }
 let ticTacToe = document.getElementsByClassName('celda')
 
 let b00 = document.getElementById("b00");
@@ -108,5 +89,4 @@ for (let index = 0; index < ticTacToe.length; index++) {
     
 // }
 }
-
 
